fed880e2f133bd5b10189b030159cd48427bf1b1
Andy Noelker <andynoelker@gmail.com>
2016-03-31 15:14:24 -0500

    Theme spec

--- test/utils/hasClass.spec.js
+++ test/utils/hasClass.spec.js
@@ -5,18 +5,18 @@ describe('hasClass', () => {
   let div = document.createElement('div')
 
   it('should detect if element contains class', () => {
-    div.className = "myClass"
-    expect(hasClass(div, 'myClass')).to.equal(true)
+    div.className = "jedi"
+    expect(hasClass(div, 'jedi')).to.equal(true)
   })
 
   it('should detect if element does not contain class', () => {
     div.className = ""
-    expect(hasClass(div, 'myClass')).to.equal(false)
+    expect(hasClass(div, 'jedi')).to.equal(false)
   })
 
   it('should detect if element with multiple classes contains class', () => {
-    div.className = "extra myClass somethingElse"
-    expect(hasClass(div, 'myClass')).to.equal(true)
+    div.className = "sith jedi bountyHunters"
+    expect(hasClass(div, 'jedi')).to.equal(true)
   })
 
 })
