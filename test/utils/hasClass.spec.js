7b2ac6407a355a11eb1cdd2da93aeafb7f14b908
Andy Noelker <andynoelker@gmail.com>
2016-04-05 11:32:59 -0500

    Add spaces

--- test/utils/hasClass.spec.js
+++ test/utils/hasClass.spec.js
@@ -4,17 +4,17 @@ import hasClass from '../../src/utils/hasClass'
 describe('hasClass', () => {
   let div = document.createElement('div')
 
-  it('should detect if element contains class', () => {
+  it ('should detect if element contains class', () => {
     div.className = "jedi"
     expect(hasClass(div, 'jedi')).to.be.true
   })
 
-  it('should detect if element does not contain class', () => {
+  it ('should detect if element does not contain class', () => {
     div.className = ""
     expect(hasClass(div, 'jedi')).to.be.false
   })
 
-  it('should detect if element with multiple classes contains class', () => {
+  it ('should detect if element with multiple classes contains class', () => {
     div.className = "sith jedi bountyHunters"
     expect(hasClass(div, 'jedi')).to.be.true
   })
