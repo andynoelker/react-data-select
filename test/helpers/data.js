9143d9b21e444118fbcf272dffaa489473ab7ba6
Andy Noelker <andynoelker@gmail.com>
2016-04-05 11:33:41 -0500

    Add more fake data

--- test/helpers/data.js
+++ test/helpers/data.js
@@ -10,7 +10,57 @@ export let fakeData = [
     firstName: 'Luke',
     lastName: 'Skywalker',
     faction: 'Jedi'
-  }
+  },
+  {
+    firstName: 'Han',
+    lastName: 'Solo',
+    faction: 'Rebels'
+  },
+  {
+    firstName: 'Leia',
+    lastName: 'Organa',
+    faction: 'Rebels'
+  },
+  {
+    firstName: 'Obi Wan',
+    lastName: 'Kenobi',
+    faction: 'Jedi'
+  },
+  {
+    firstName: 'Mace',
+    lastName: 'Windu',
+    faction: 'Jedi'
+  },
+  {
+    firstName: 'Darth',
+    lastName: 'Maul',
+    faction: 'Sith'
+  },
+  {
+    firstName: 'Kylo',
+    lastName: 'Ren',
+    faction: 'Sith'
+  },
+  {
+    firstName: 'Boba',
+    lastName: 'Fett',
+    faction: 'Bounty hunters'
+  },
+  {
+    firstName: 'Lando',
+    lastName: 'Calrissian',
+    faction: 'Rebels'
+  },
+  {
+    firstName: 'Qui-Gon',
+    lastName: 'Jinn',
+    faction: 'Jedi'
+  },
+  {
+    firstName: 'Count',
+    lastName: 'Dooku',
+    faction: 'Sith'
+  },
 ]
 
 export let fakeImmutableData = Immutable.fromJS([
@@ -23,5 +73,55 @@ export let fakeImmutableData = Immutable.fromJS([
     firstName: 'Luke',
     lastName: 'Skywalker',
     faction: 'Jedi'
-  }
+  },
+  {
+    firstName: 'Han',
+    lastName: 'Solo',
+    faction: 'Rebels'
+  },
+  {
+    firstName: 'Leia',
+    lastName: 'Organa',
+    faction: 'Rebels'
+  },
+  {
+    firstName: 'Obi Wan',
+    lastName: 'Kenobi',
+    faction: 'Jedi'
+  },
+  {
+    firstName: 'Mace',
+    lastName: 'Windu',
+    faction: 'Jedi'
+  },
+  {
+    firstName: 'Darth',
+    lastName: 'Maul',
+    faction: 'Sith'
+  },
+  {
+    firstName: 'Kylo',
+    lastName: 'Ren',
+    faction: 'Sith'
+  },
+  {
+    firstName: 'Boba',
+    lastName: 'Fett',
+    faction: 'Bounty hunters'
+  },
+  {
+    firstName: 'Lando',
+    lastName: 'Calrissian',
+    faction: 'Rebels'
+  },
+  {
+    firstName: 'Qui-Gon',
+    lastName: 'Jinn',
+    faction: 'Jedi'
+  },
+  {
+    firstName: 'Count',
+    lastName: 'Dooku',
+    faction: 'Sith'
+  },
 ])
